c6f7a7e3b1e7a63d48e9f251ec13316c66b9ef81
guanwanxiao <18257132004@163.com>
2019-06-17 08:41:09 +0800

    swiper vertical

--- minprogram/pages/index/index.js
+++ minprogram/pages/index/index.js
@@ -3,7 +3,16 @@ import { LOGIN_TOKEN } from '../../utils/localStorage'
 Page({
   data: {
     token: '',
-    list: []
+    list: [],
+    imgUrls: [
+      'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+      'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
+      'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640'
+    ],
+    indicatorDots: false,
+    autoplay: true,
+    interval: 2000,
+    duration: 1000
   },
   //事件处理函数
   bindViewTap: function() {
